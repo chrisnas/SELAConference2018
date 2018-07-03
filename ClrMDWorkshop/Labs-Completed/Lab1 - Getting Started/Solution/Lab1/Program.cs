f2aea85656a62c5bf78fd76e4bdae24e89646047
Christophe Nasarre <c.nasarresoulier@criteo.com>
2018-07-03 11:13:03 +0300

    Add attach to process support

--- ClrMDWorkshop/Labs-Completed/Lab1 - Getting Started/Solution/Lab1/Program.cs
+++ ClrMDWorkshop/Labs-Completed/Lab1 - Getting Started/Solution/Lab1/Program.cs
@@ -9,15 +9,21 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            // args[0] is supposed to be the dump filename
+            // args[0] is supposed to be the dump filename or a process id
 
             if (args.Length != 1)
             {
-                Console.WriteLine("A dump filename must be provided.");
+                Console.WriteLine("A dump filename or a process id must be provided.");
                 return;
             }
 
             var dumpFilename = args[0];
+            if (int.TryParse(args[0], out int pid))
+            {
+                AttachToLiveProcess(pid);
+                return;
+            }
+
             if (!File.Exists(dumpFilename))
             {
                 Console.WriteLine($"{dumpFilename} does not exist.");
@@ -27,35 +33,48 @@ namespace Lab1
             ProcessDumpFile(args[0]);
         }
 
+        private static void AttachToLiveProcess(int pid)
+        {
+            using (var target = DataTarget.AttachToProcess(pid, 2000))
+            {
+                AnalyzeApplication(target);
+            }
+        }
+
         private static void ProcessDumpFile(string dumpFilename)
         {
             using (var target = Utils.GetTarget(dumpFilename))
             {
+                AnalyzeApplication(target);
+            }
+        }
+
+        private static void AnalyzeApplication(DataTarget target)
+        {
+            try
+            {
                 ClrRuntime clr = null;
 
-                try
-                {
-                    // get the bitness of the application in two different ways
-                    Console.WriteLine(target.Architecture.ToString());
-                    Console.WriteLine((target.PointerSize == 4) ? "32 bit" : "64 bit");
+                // get the bitness of the application in two different ways
+                Console.WriteLine(target.Architecture.ToString());
+                Console.WriteLine((target.PointerSize == 4) ? "32 bit" : "64 bit");
 
-                    // At this point, it is expected that the current tool and the dump are sharing the same bitness
-                    // or an exception will be thrown
+                // At this point, it is expected that the current tool and the dump are sharing the same bitness
+                // or an exception will be thrown
 
-                    // if you were able to get the mscordacwks.dll of the machine on which the dump 
-                    // was taken, pass it to the CreateRuntime(string) overwrite
-                    clr = target.ClrVersions[0].CreateRuntime();
+                // if you were able to get the mscordacwks.dll of the machine on which the dump
+                // was taken, pass it to the CreateRuntime(string) overwrite
+                clr = target.ClrVersions[0].CreateRuntime();
 
-                    // get the version of the CLR in two different ways
-                    Console.WriteLine(target.ClrVersions[0].Version.ToString());
-                    Console.WriteLine(clr.ClrInfo.Version.ToString());
+                // get the version of the CLR in two different ways
+                Console.WriteLine(target.ClrVersions[0].Version.ToString());
+                Console.WriteLine(clr.ClrInfo.Version.ToString());
 
-                    ListLoadedAssemblies(clr);
-                }
-                catch (Exception x)
-                {
-                    Console.WriteLine(x.Message);
-                }
+                ListLoadedAssemblies(clr);
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
             }
         }
 
@@ -71,7 +90,7 @@ namespace Lab1
                 Console.WriteLine(module.FileName);
             }
 
-            // ... or use the ClrRuntime instance 
+            // ... or use the ClrRuntime instance
             // (remember that an assembly can contain several modules in .NET)
             foreach (var module in clr.Modules)
             {
